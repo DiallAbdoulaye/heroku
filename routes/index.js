514b9edb05c3c519b1a0bc8dc092856f7d764228
DiallAbdoulaye <diallo.a6@gmail.com>
2018-03-09 13:41:40 +0100

    Modif message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ 'hi': 'Hello dude' });
+  res.json({ 'hi': 'Hello World, now youre mine !' });
 });
 
 module.exports = router;
